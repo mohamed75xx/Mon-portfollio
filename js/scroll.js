24a6dc7583973ace59689fb1aef046a25c513a56
Mohamed <achour_m@etna-alternance.net>
2023-01-16 14:19:43 +0100

    MP

---
+++ js/scroll.js
@@ -0,0 +1,56 @@
+const btn1 = document.querySelector('.HOME');
+
+btn1.addEventListener('click', () => {
+
+    window.scrollTo({
+        top:0,
+        left: 0,
+        behavior:"smooth"
+    })
+})
+
+const btn2 = document.querySelector('.ABOUTME');
+
+btn2.addEventListener('click', () => {
+
+    window.scrollTo({
+        top:150,
+        left: 0,
+        behavior:"smooth"
+    })
+})
+
+const btn3 = document.querySelector('.RESUME');
+
+btn3.addEventListener('click', () => {
+
+    window.scrollTo({
+        top:1150,
+        left: 0,
+        behavior:"smooth"
+    })
+})
+
+const btn4 = document.querySelector('.MYPORTFOLLIO');
+
+btn4.addEventListener('click', () => {
+
+    window.scrollTo({
+        top:1990,
+        left: 0,
+        behavior:"smooth"
+    })
+})
+
+
+
+const btn5 = document.querySelector('.CONTACTME');
+
+btn5.addEventListener('click', () => {
+
+    window.scrollTo({
+        top:3000,
+        left: 0,
+        behavior:"smooth"
+    })
+})
